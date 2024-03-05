220358766287215a7ed825741e84968335598380
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-03-05 13:18:10 +0100

    version bump

--- qlasskit/__init__.py
+++ qlasskit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # isort:skip_file
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 from .qcircuit import QCircuit, SupportedFrameworks, SupportedFramework  # noqa: F401
 from .qlassfun import QlassF, qlassf, qlassfa  # noqa: F401
