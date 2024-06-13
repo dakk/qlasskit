250af574597710dd7040c8794585ce3c43ff6331
Divyanshu Singh <55018955+divshacker@users.noreply.github.com>
2024-06-13 20:10:59 +0530

    New Algorithm - Bernstein-Vazirani  (#55)

---
+++ qlasskit/algorithms/bernsteinvazirani.py
@@ -0,0 +1,73 @@
+# Copyright 2023-2024 Davide Gessa
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List, Tuple, Union
+
+from ..qcircuit import QCircuit
+from ..qlassfun import QlassF
+from ..types import Qtype, interpret_as_qtype, format_outcome
+from .qalgorithm import QAlgorithm
+
+
+class BernsteinVazirani(QAlgorithm):
+    def __init__(
+        self,
+        f: QlassF,
+    ):
+        """
+        Args:
+            f (QlassF): our f(x) -> bool
+        """
+        if len(f.args) != 1:
+            raise Exception("f should receive exactly one parameter")
+        if f.returns.ttype != bool:
+            raise Exception("f should returns bool")
+
+        self.f: QlassF = f
+        self.search_space_size = len(f.args[0])
+        self._qcircuit = QCircuit(self.f.num_qubits, name=f"deutsch_{f.name}")
+
+        self._f_circuit = self.f.circuit()
+
+        # State preparation
+        self._qcircuit.barrier(label="s")
+        for i in range(self.search_space_size):
+            self._qcircuit.h(i)
+        self._qcircuit.h(self._f_circuit["_ret"])
+        self._qcircuit.z(self._f_circuit["_ret"])
+
+        # Prepare and add the f
+        self._qcircuit.barrier(label="f")
+        self._qcircuit += self._f_circuit
+
+        # State preparation out
+        self._qcircuit.barrier(label="s")
+        for i in range(self.search_space_size):
+            self._qcircuit.h(i)
+
+    # @override
+    @property
+    def output_qubits(self) -> List[int]:
+        """Returns the list of output qubits"""
+        len_a = len(self.f.args[0])
+        return list(range(len_a))
+        
+
+
+    # @override
+    def decode_output(
+        self, istr: Union[str, int, List, dict]
+    ) -> Union[Tuple[str, int], Qtype, str]: 
+        format_outcome(istr, len(self.f.args[0]) - 1)
+        return istr[-len(self.f.args[0]):][::-1]
---
+++ test/algo/test_bernstein_vazirani.py
@@ -0,0 +1,69 @@
+# Copyright 2023-2024 Davide Gessa
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from parameterized import parameterized_class
+
+from qlasskit import qlassf
+from qlasskit.algorithms import BernsteinVazirani
+
+from ..utils import ENABLED_COMPILERS, qiskit_measure_and_count
+
+
+@parameterized_class(("compiler"), ENABLED_COMPILERS)
+class TestAlgoBernsteinVazirani(unittest.TestCase):
+    
+
+    def test_1_bernstein_vazirani(self):
+        f = """
+def oracle(x: Qint[4]) -> bool:
+        s=Qint4(14)
+        return ((x[0]&s[0])^(x[1]&s[1])^(x[2]&s[2])^(x[3]&s[3]))
+"""
+        qf = qlassf(f, compiler=self.compiler)
+        algo = BernsteinVazirani(qf)
+
+        qc_algo = algo.circuit().export("circuit", "qiskit")
+        counts = qiskit_measure_and_count(qc_algo, shots=1024)
+        counts_readable = algo.decode_counts(counts)
+        self.assertEqual(counts_readable["0111"],1024)
+
+    def test_2_bernstein_vazirani(self):
+        f = """
+def oracle(x: Qint[4]) -> bool:
+        s=Qint4(12)
+        return ((x[0]&s[0])^(x[1]&s[1])^(x[2]&s[2])^(x[3]&s[3]))
+"""
+        qf = qlassf(f, compiler=self.compiler)
+        algo = BernsteinVazirani(qf)
+
+        qc_algo = algo.circuit().export("circuit", "qiskit")
+        counts = qiskit_measure_and_count(qc_algo, shots=1024)
+        counts_readable = algo.decode_counts(counts)
+        self.assertEqual(counts_readable["0011"],1024)
+
+    def test_3_bernstein_vazirani(self):
+        f = """
+def oracle(x: Qint[4]) -> bool:
+        s=Qint4(15)
+        return ((x[0]&s[0])^(x[1]&s[1])^(x[2]&s[2])^(x[3]&s[3]))
+"""
+        qf = qlassf(f, compiler=self.compiler)
+        algo = BernsteinVazirani(qf)
+
+        qc_algo = algo.circuit().export("circuit", "qiskit")
+        counts = qiskit_measure_and_count(qc_algo, shots=1024)
+        counts_readable = algo.decode_counts(counts)
+        self.assertEqual(counts_readable["1111"],1024)
