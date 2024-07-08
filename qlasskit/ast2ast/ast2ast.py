af1d01396411442f53a683c1144ae807b1c72b95
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-07-08 18:32:35 +0200

    add a constant folding step after ast rewriting

--- qlasskit/ast2ast/ast2ast.py
+++ qlasskit/ast2ast/ast2ast.py
@@ -49,5 +49,8 @@ def ast2ast(a_tree):
     # Rewrite the ast
     a_tree = ASTRewriter().visit(a_tree)
 
+    # Fold constants again
+    a_tree = ConstantFolder().visit(a_tree)
+    
     # print(ast.dump(a_tree))
     return a_tree
