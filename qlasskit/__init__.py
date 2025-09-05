7439118e715b8119e9961aaad9974be429bc661c
Davide Gessa <gessadavide@gmail.com>
2025-09-05 09:11:52 +0200

    update version

--- qlasskit/__init__.py
+++ qlasskit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # isort:skip_file
 
-__version__ = "0.1.35"
+__version__ = "0.1.36"
 
 from .qcircuit import QCircuit, SupportedFrameworks, SupportedFramework  # noqa: F401
 from .qlassfun import QlassF, qlassf, qlassfa  # noqa: F401
